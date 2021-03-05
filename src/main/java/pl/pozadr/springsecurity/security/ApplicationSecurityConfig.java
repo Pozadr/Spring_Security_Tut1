12080dd91dc987ccb23b87450849bd90c1c8e75b
Pozadr <adpozor@gmail.com>
2021-03-05 14:24:51 +0100

    authoritaties

--- src/main/java/pl/pozadr/springsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/pl/pozadr/springsecurity/security/ApplicationSecurityConfig.java
@@ -3,15 +3,18 @@ package pl.pozadr.springsecurity.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import static pl.pozadr.springsecurity.security.ApplicationUserPermission.*;
 import static pl.pozadr.springsecurity.security.ApplicationUserRole.*;
 
 @Configuration
@@ -28,9 +31,16 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
                 .antMatchers("/api/**").hasRole(STUDENT.name())
+
+                .antMatchers(HttpMethod.DELETE,"/management/api/**").hasAuthority(STUDENT_WRITE.getPermission())
+                .antMatchers(HttpMethod.POST,"/management/api/**").hasAuthority(STUDENT_WRITE.getPermission())
+                .antMatchers(HttpMethod.PUT,"/management/api/**").hasAuthority(STUDENT_WRITE.getPermission())
+                .antMatchers(HttpMethod.GET,"/management/api/**").hasAnyRole(ADMIN.name(), ADMIN_TRAINEE.name())
+
                 .anyRequest()
                 .authenticated()
                 .and()
@@ -40,21 +50,31 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     @Bean
     protected UserDetailsService userDetailsService() {
-        UserDetails annaSmith = User.builder()
+        UserDetails annaSmithUser = User.builder()
                 .username("annasmith")
                 .password(passwordEncoder.encode("password"))
-                .roles(STUDENT.name())
+//                .roles(STUDENT.name())
+                .authorities(STUDENT.getGrantedAuthorities())
                 .build();
 
-        UserDetails admin = User.builder()
+        UserDetails adrianAdminUser = User.builder()
                 .username("adrian")
                 .password(passwordEncoder.encode("password123"))
-                .roles(ADMIN.name())
+//                .roles(ADMIN.name())
+                .authorities(ADMIN.getGrantedAuthorities())
+                .build();
+
+        UserDetails tomAdminTraineeUser = User.builder()
+                .username("tom")
+                .password(passwordEncoder.encode("password123"))
+//                .roles(ADMIN_TRAINEE.name())
+                .authorities(ADMIN_TRAINEE.getGrantedAuthorities())
                 .build();
 
         return new InMemoryUserDetailsManager(
-                admin,
-                annaSmith
+                adrianAdminUser,
+                tomAdminTraineeUser,
+                annaSmithUser
         );
 
     }
--- src/main/java/pl/pozadr/springsecurity/security/ApplicationUserRole.java
+++ src/main/java/pl/pozadr/springsecurity/security/ApplicationUserRole.java
@@ -2,13 +2,16 @@ package pl.pozadr.springsecurity.security;
 
 
 import com.google.common.collect.Sets;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import static pl.pozadr.springsecurity.security.ApplicationUserPermission.*;
 
 public enum ApplicationUserRole {
     ADMIN(Sets.newHashSet(STUDENT_READ, STUDENT_WRITE, COURSE_READ, COURSE_WRITE)),
+    ADMIN_TRAINEE(Sets.newHashSet(STUDENT_READ, COURSE_READ)),
     STUDENT(Sets.newHashSet());
 
     private final Set<ApplicationUserPermission> permissions;
@@ -18,6 +21,14 @@ public enum ApplicationUserRole {
     }
 
     public Set<ApplicationUserPermission> getPermissions() {
+        return permissions;
+    }
+
+    public Set<SimpleGrantedAuthority> getGrantedAuthorities() {
+        Set<SimpleGrantedAuthority> permissions = getPermissions().stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.name()))
+                .collect(Collectors.toSet());
+        permissions.add(new SimpleGrantedAuthority(("ROLE_" + this.name())));
         return permissions;
     }
 }
